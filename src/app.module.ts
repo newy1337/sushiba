2262424dc99583f854362c3b9ed5b5ef820d2cdf
newy <newy1337@gmail.com>
2024-07-05 17:53:45 +0100

    added working days

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { OrderModule } from './order/order.module';
+import { SettingsModule } from './settings/settings.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { OrderModule } from './order/order.module';
     ProductModule,
     UserModule,
     OrderModule,
+    SettingsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
