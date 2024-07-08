37c042cb077c9aa81f3044975c57996888573de6
NEWY-PC <newy1337@gmail.com>
2024-07-08 20:37:11 +0100

    mbway fix

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -101,7 +101,7 @@ export class OrderService {
 
     try {
       const session = await this.stripe.checkout.sessions.create({
-        payment_method_types: ['card', 'multibanco', 'paypal'],
+        payment_method_types: ['card', 'multibanco'],
         line_items: dto.items.map((item) => ({
           price_data: {
             currency: 'eur',
