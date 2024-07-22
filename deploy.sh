4809e8f26aa5f872636673a3a747c7c638269fdd
newy <newy1337@gmail.com>
2024-07-22 12:23:32 +0100

    deploy file fix

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 cd sushiba
 git pull origin master
 # Добавьте команды для сборки и перезапуска вашего приложения, например:
-#npm install
-#pm2 restart your-app-name
+pnpm install
+pm2 restart sushiba-back
