2d763ee3cae8282d6006b772b1812d9519e999a8
NEWY-PC <newy1337@gmail.com>
2024-09-14 14:49:12 +0100

    order time debug

--- src/modules/order/validators/custom/validate.order.ts
+++ src/modules/order/validators/custom/validate.order.ts
@@ -8,23 +8,22 @@ export const isAvailableOrderNow = async (
 ) => {
   try {
     const portugalTimezone = 'Europe/Lisbon';
-
     const preorder = dto.details.preorder;
-    const dayName = preorder
-      ? moment(dto.details.preorderTime).tz(portugalTimezone).format('dddd')
-      : moment().tz(portugalTimezone).format('dddd');
 
+    // Определение времени заказа
     const orderTime = preorder
       ? moment(dto.details.preorderTime).tz(portugalTimezone)
-      : moment().tz(portugalTimezone).format('HH:mm');
-    console.log('order_time:' + orderTime);
-    const orderTimeOnly = moment(orderTime)
-      .set({
-        year: 1970,
-        month: 0,
-        date: 1,
-      })
-      .tz(portugalTimezone);
+      : moment().tz(portugalTimezone);
+
+    const dayName = preorder
+      ? orderTime.format('dddd')
+      : moment().tz(portugalTimezone).format('dddd');
+
+    const orderTimeOnly = orderTime.clone().set({
+      year: 1970,
+      month: 0,
+      date: 1,
+    });
 
     const workingHours = await prisma.workingHours.findUnique({
       where: {
@@ -37,32 +36,34 @@ export const isAvailableOrderNow = async (
     }
 
     let isAvailable = false;
-    if (dto.details.deliveryMethod === 'taxiDelivery') {
-      const deliveryStartTime = moment(workingHours.deliveryStart, 'HH:mm')
-        .tz(portugalTimezone)
-        .set({ year: 1970, month: 0, date: 1 });
-      const deliveryEndTime = moment(workingHours.deliveryEnd, 'HH:mm')
-        .tz(portugalTimezone)
-        .set({ year: 1970, month: 0, date: 1 });
-      const lastAvailableTime = moment(deliveryEndTime).subtract(30, 'minutes');
 
-      console.log(deliveryStartTime, deliveryEndTime);
+    // Проверка доступности для доставки
+    if (dto.details.deliveryMethod === 'taxiDelivery') {
+      const deliveryStartTime = moment
+        .tz(workingHours.deliveryStart, 'HH:mm', portugalTimezone)
+        .set({ year: 1970, month: 0, date: 1 });
+      const deliveryEndTime = moment
+        .tz(workingHours.deliveryEnd, 'HH:mm', portugalTimezone)
+        .set({ year: 1970, month: 0, date: 1 });
+      const lastAvailableTime = deliveryEndTime.clone().subtract(30, 'minutes');
+
       isAvailable = orderTimeOnly.isBetween(
         deliveryStartTime,
         preorder ? deliveryEndTime : lastAvailableTime,
         undefined,
         '[]',
       );
-    } else if (dto.details.deliveryMethod === 'takeAway') {
-      const takeawayStartTime = moment(workingHours.takeawayStart, 'HH:mm')
-        .tz(portugalTimezone)
+    }
+    // Проверка доступности для самовывоза
+    else if (dto.details.deliveryMethod === 'takeAway') {
+      const takeawayStartTime = moment
+        .tz(workingHours.takeawayStart, 'HH:mm', portugalTimezone)
         .set({ year: 1970, month: 0, date: 1 });
-      const takeawayEndTime = moment(workingHours.takeawayEnd, 'HH:mm')
-        .tz(portugalTimezone)
+      const takeawayEndTime = moment
+        .tz(workingHours.takeawayEnd, 'HH:mm', portugalTimezone)
         .set({ year: 1970, month: 0, date: 1 });
-      const lastAvailableTime = moment(takeawayEndTime).subtract(30, 'minutes');
+      const lastAvailableTime = takeawayEndTime.clone().subtract(30, 'minutes');
 
-      console.log(takeawayStartTime, orderTimeOnly, takeawayEndTime);
       isAvailable = orderTimeOnly.isBetween(
         takeawayStartTime,
         preorder ? takeawayEndTime : lastAvailableTime,
